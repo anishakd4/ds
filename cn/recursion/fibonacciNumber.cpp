c2a8fc7bd540fb9812936210e2a90a91bb5007a3
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-2.local>
2022-01-26 19:34:50 +0530

    init

---
+++ cn/recursion/fibonacciNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <climits>
+// climits for INT_MIN
+#include <unordered_map>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int i;
+    cin >> i;
+    cout << fib(i) << endl;
+    return 0;
+}
