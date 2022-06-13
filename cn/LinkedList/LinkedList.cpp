bcd0deabc7eb66a3019a9868265c9a6e75099212
Anish <kumaranishdubey@gmail.com>
2022-06-13 18:43:26 +0530

    init

--- cn/LinkedList/LinkedList.cpp
+++ cn/LinkedList/LinkedList.cpp
@@ -11,9 +11,177 @@
 #include <unordered_map>
 using namespace std;
 #include "Node.cpp"
+
+/*
+Time complexity: O(n^2)
+
+n   ->  Time complexity
+1 -> k
+2 ->1
+3 ->2
+4 ->3
+5 ->4
+......
+n ->n-1
+
+Time complexity: O(n^2)
+*/
+
+// Node *takeInput()
+// {
+//     int data;
+//     cin >> data;
+//     Node *head = NULL;
+//     while (data != -1)
+//     {
+//         Node *newNode = new Node(data);
+//         if (head == NULL)
+//         {
+//             head = newNode;
+//         }
+//         else
+//         {
+//             Node *temp = head;
+//             while (temp->next != NULL)
+//             {
+//                 temp = temp->next;
+//             }
+//             temp->next = newNode;
+//         }
+//         cin >> data;
+//     }
+//     return head;
+// }
+
+// Better time complexity as for each new node we are doing constant operations
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (data != -1)
+    {
+        // Node newNode(data); this will create issue as static memory allocation is scope specific
+        // The memory will be deallocated after each iteration
+        Node *newNode = new Node(data); // while dynamic memory allocation's deallocation is done manually.
+        // These are not deallocated until we deallocate them explicitly
+        if (head == NULL)
+        {
+            // head = &newNode;
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+            // OR tail=newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+/*
+
+    newNode
+    _________________           _____________________
+    |       100     |           |     10   |  NULL  | address=100   These are not deallocated after iteration
+    |_______________|           |__________|________|               as these are dynamic memory allocation
+    deallocated after
+    iteration as static
+    memory
+
+    newNode
+    _________________           _____________________
+    |       100     |           |     10   |  NULL  | address=100
+    |_______________|           |__________|________|
+    deallocated after
+    iteration as static
+    memory
+
+    newNode
+    _________________           _____________________
+    |       100     |           |     10   |  NULL  | address=100
+    |_______________|           |__________|________|
+    deallocated after
+    iteration as static
+    memory
+
+*/
+
+Node *insertNode(Node *head, int i, int data)
+{
+    Node *newNode = new Node(data);
+    int count = 0;
+    Node *temp = head;
+    if (i == 0)
+    {
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }
+    // temp != NULL for condition when i > size of the linked list and thus to avoid segmentation fault
+    while (temp != NULL && count < i - 1)
+    {
+        count++;
+        temp = temp->next;
+    }
+    if (temp != NULL)
+    {
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
 int main()
 {
     // statically initialize
     Node n1(1);
+    Node *head = &n1;
+    Node n2(2);
+    n1.next = &n2;
+    cout << n1.data << " " << n2.data << endl;
+
+    // Dynamically initialize
+    Node *n3 = new Node(10);
+    Node *head2 = n3;
+    Node *n4 = new Node(20);
+    n3->next = n4;
+
+    // create linked list of 5 elements
+    Node a(1);
+    Node *head3 = &a;
+    Node b(2);
+    Node c(3);
+    Node d(4);
+    Node e(5);
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
+    print(head3);
+
+    // take input
+    Node *head4 = takeInput();
+    print(head4);
+
+    head4 = insertNode(head4, 0, 99);
+    print(head4);
+
     return 0;
 }
+
+// First node of linked list is called Head node and last node is called Tail node.
+// We save the address of Head node, if we loose it we will loose the whole linked list
