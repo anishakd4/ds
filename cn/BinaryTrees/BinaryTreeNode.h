8aa8dd1b32fce23bf33acc420cecc93e97e112a7
Anish <kumaranishdubey@gmail.com>
2022-06-18 16:31:41 +0530

    init

---
+++ cn/BinaryTrees/BinaryTreeNode.h
@@ -0,0 +1,21 @@
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode()
+    {
+        delete left; /// if left is NULL, then delete NULL is fine. It will be ignored and ait won't create error.
+        delete right;
+    }
+};
