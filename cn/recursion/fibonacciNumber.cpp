16d7dc3a7442893ee8890f5914f25ab0b57a7c7a
Anish <kumaranishdubey@gmail.com>
2022-06-28 07:57:50 +0530

    init

--- cn/recursion/fibonacciNumber.cpp
+++ cn/recursion/fibonacciNumber.cpp
@@ -1,3 +1,36 @@
+/*
+Calculate nth fibonacci number.
+
+The Fibonacci numbers are the numbers in the following integer sequence.
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ....
+
+In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation
+    Fn = Fn-1 + Fn-2
+
+with seed values
+    F0 = 0 and F1 = 1.
+
+Input Format:
+    A single integer ‘N’
+
+Output Format:
+    A single integer representing nth fibonacci number
+
+Constraints:
+    1 <= N <= 10^5
+
+Sample Input :
+    n = 2
+
+Sample Output :
+    1
+
+Sample Input :
+    n = 9
+
+Sample Output :
+    34
+*/
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
