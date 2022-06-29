c4230604c688a8a90c643a53f04cc96d5821a192
Anish <kumaranishdubey@gmail.com>
2022-06-29 18:01:20 +0530

    init

--- cn/recursion/staircaseRecursion.cpp
+++ cn/recursion/staircaseRecursion.cpp
@@ -1,3 +1,28 @@
+/*
+A child is running up a staircase with N steps, and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count
+how many possible ways the child can run up to the stairs. You need to return number of possible ways W.
+
+Input format :
+    Integer N
+
+Output Format :
+    Integer W
+
+Constraints :
+    1 <= N <= 30
+
+Sample Input 1 :
+    4
+
+Sample Output 1 :
+    7
+
+Sample Input 2 :
+    5
+
+Sample Output 2 :
+    13
+*/
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
@@ -17,12 +42,13 @@ int staircase(int n)
     {
         return 1;
     }
-    if (n < 1)
+    if (n < 0)
     {
         return 0;
     }
     return staircase(n - 1) + staircase(n - 2) + staircase(n - 3);
 }
+
 int main()
 {
     int n, output;
